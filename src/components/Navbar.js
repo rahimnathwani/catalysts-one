d5bcac26608a2454e48c0be94f05466bab4a2986
Rahim Nathwani <rahim@encona.com>
2020-09-23 23:23:55 -0700

    Increase logo size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="CatalystsOne" style={{ width: '224px' }} />
+              <img src={logo} alt="CatalystsOne" style={{ width: '320px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
